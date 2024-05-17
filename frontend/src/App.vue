8e401f24e281f5069ebeea71b745055eb8ce4eb4
seejn <seejnmaharjan@gmail.com>
2024-05-17 17:20:57 +0545

    frontend initialized

---
+++ frontend/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <!-- <CreatePost /> -->
+  </div>
+</template>
+
+<script>
+
+  import Card from './components/posts/Card.vue'
+  import Comment from './components/posts/Comment.vue'
+  import FullViewPost from './components/posts/FullViewPost.vue'
+  import CreatePost from './components/posts/CreatePost.vue'
+
+  import Profile from './components/user/Profile.vue'
+  import EditProfile from './components/user/EditProfile.vue'
+  import SignIn from './pages/SignIn.vue'
+  import SignUp from './pages/SignUp.vue'
+  import Alert from './components/Alert.vue'
+  
+  import Navbar from './components/Navbar/NavBar.vue'
+  import UserNavbar from './components/Navbar/UserNavBar.vue'
+  
+  export default {
+    components: {
+
+      Navbar,
+      UserNavbar,
+      
+      Card,
+      Comment,
+      FullViewPost,
+      CreatePost,
+
+      Profile,
+      SignUp,
+      EditProfile,
+      SignIn,
+      Alert,
+
+    },
+    computed: {
+      isUserLoggedIn() {
+        // return this.$store.
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,38 @@
+import { createApp } from 'vue'
+import { createWebHistory, createRouter } from 'vue-router'
+
+import './style.css'
+import App from './App.vue'
+import store from './store/store.js'
+
+import SignOut from './components/user/SignOut.vue'
+
+import LandingPage from './pages/LandingPage.vue'
+import Dashboard from './pages/Dashboard.vue'
+import SignIn from './pages/SignIn.vue'
+import SignUp from './pages/SignUp.vue'
+import CreatePost from './pages/CreatePost.vue'
+import Profile from './pages/Profile.vue'
+
+import Toasts from './components/toasts/Toasts.vue'
+
+const routes = [
+    { path: '/', component: LandingPage },
+    { path: '/signin', component: SignIn },
+    { path: '/signup', component: SignUp },
+    { path: '/signout', component: SignOut },
+    { path: '/dashboard', component: Dashboard },
+    { path: '/create-post', component: CreatePost },
+    { path: '/profile', component: Profile },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+ })
+
+const app = createApp(App)
+app.component("Toasts", Toasts)
+app.use(router)
+app.use(store)
+app.mount('#app')
